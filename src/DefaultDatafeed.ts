41cda0fe38cb99391ecb16b0c80091e84b036103
Michal Gally <michal.gally@icloud.com>
2025-10-27 11:30:58 +0100

    chore: update version to 0.2.2 and modify getSymbolDetails method to accept string parameter

--- src/DefaultDatafeed.ts
+++ src/DefaultDatafeed.ts
@@ -43,8 +43,8 @@ export default class DefaultDatafeed implements Datafeed {
     }))
   }
 
-  async getSymbolDetails(symbol: SymbolInfo): Promise<SymbolDetails> {
-    const response = await fetch(`https://api.polygon.io/v3/reference/tickers?apiKey=${this._apiKey}&active=true&ticker=${symbol.ticker}`)
+  async getSymbolDetails(symbol: string): Promise<SymbolDetails> {
+    const response = await fetch(`https://api.polygon.io/v3/reference/tickers?apiKey=${this._apiKey}&active=true&ticker=${symbol}`)
     const result = await response.json()
     return await (result.results || []).map((data: any) => ({
       ticker: data.ticker,
